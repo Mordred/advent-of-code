1ae2d50f222a6cc2b38aca233d8d19c9a0ab0ac1
Martin Jantosovic <jantosovic.martin@gmail.com>
2021-12-19 22:07:26 +0100

    perf(19): Optimize Day 19 with generators

--- 2021/19/index.ts
+++ 2021/19/index.ts
@@ -2,71 +2,65 @@ import fs from 'fs/promises';
 
 type Point = number[];
 
-const DIM = 3;
-
 const toInt = (v: string) => parseInt(v, 10);
 
-function rebase(beacons: Point[], base: Point): Point[] {
-  return beacons.map((p) => p.map((v, i) => v - base[i]));
+function* rebase(beacons: Point[], base: Point): Generator<Point> {
+  for (const beacon of beacons) {
+    yield beacon.map((v, i) => v - base[i]);
+  }
 }
 
 const key = (point: Point) => point.join(',');
 
 const ROTATIONS = [
-  [1, 1, 1],
-  [-1, 1, 1],
-  [1, -1, 1],
-  [1, 1, -1],
-  [1, -1, -1],
-  [-1, 1, -1],
-  [-1, -1, 1],
-  [-1, -1, -1],
-];
+  // 90 deg around X
+  ([x, y, z]) => [x, y, z],
+  ([x, y, z]) => [x, z, -y],
+  ([x, y, z]) => [x, -y, -z],
+  ([x, y, z]) => [x, -z, y],
+  // 90 deg around -X
+  ([x, y, z]) => [-x, z, y],
+  ([x, y, z]) => [-x, y, -z],
+  ([x, y, z]) => [-x, -z, -y],
+  ([x, y, z]) => [-x, -y, z],
+  // 90 deg around Y
+  ([x, y, z]) => [y, z, x],
+  ([x, y, z]) => [y, x, -z],
+  ([x, y, z]) => [y, -z, -x],
+  ([x, y, z]) => [y, -x, z],
+  // 90 deg around -Y
+  ([x, y, z]) => [-y, x, z],
+  ([x, y, z]) => [-y, z, -x],
+  ([x, y, z]) => [-y, -x, -z],
+  ([x, y, z]) => [-y, -z, x],
+  // 90 deg around Z
+  ([x, y, z]) => [z, x, y],
+  ([x, y, z]) => [z, y, -x],
+  ([x, y, z]) => [z, -x, -y],
+  ([x, y, z]) => [z, -y, x],
+  // 90 deg around -Z
+  ([x, y, z]) => [-z, y, x],
+  ([x, y, z]) => [-z, x, -y],
+  ([x, y, z]) => [-z, -y, -x],
+  ([x, y, z]) => [-z, -x, y],
+]
+
 function* rotations(beacons: Point[]) {
   for (const r of ROTATIONS) {
-    // Current: x, y, z
-    yield beacons.map((p) => [p[0] * r[0], p[1] * r[1], p[2] * r[2]]);
-    // Swap x -> z: z, y, x
-    yield beacons.map((p) => [p[2] * r[2], p[1] * r[1], p[0] * r[0]]);
-    // Swap y -> z: x, z, y
-    yield beacons.map((p) => [p[0] * r[0], p[2] * r[2], p[1] * r[1]]);
-
-    // Swap x -> y: y, x, z
-    yield beacons.map((p) => [p[1] * r[1], p[0] * r[0], p[2] * r[2]]);
-    // Swap x -> y: y, z, x
-    yield beacons.map((p) => [p[1] * r[1], p[2] * r[2], p[0] * r[0]]);
-    // Swap x -> y: z, x, y
-    yield beacons.map((p) => [p[2] * r[2], p[0] * r[0], p[1] * r[1]]);
+    yield beacons.map(r);
   }
-}
-
-function intersection(aBeacons: Point[], bBeacons: Point[]): Point[] {
-  const inter = {};
-  const result: Point[] = [];
-  for (const b of aBeacons) {
-    inter[key(b)] = b;
-  }
-  for (const b of bBeacons) {
-    if (inter[key(b)]) {
-      result.push(b);
-    }
-  }
-  return result;
-}
-
-function union(aBeacons: Point[], bBeacons: Point[]): Point[] {
-  const union = {};
-  for (const b of aBeacons) {
-    union[key(b)] = b;
-  }
-  for (const b of bBeacons) {
-    union[key(b)] = b;
-  }
-  return Array.from(Object.values(union));
 }
 
 function distance(a: Point, b: Point): number {
   return a.reduce((acc, cur, i) => acc + Math.abs(cur - b[i]), 0);
+}
+
+function toMap(beacons: Point[] | Generator<Point>): Record<string, Point> {
+  const ret = {};
+  for (const beacon of beacons) {
+    ret[key(beacon)] = beacon;
+  }
+  return ret;
 }
 
 async function run() {
@@ -81,24 +75,31 @@ async function run() {
         .map((c) => c.split(',').map(toInt) as Point),
     );
 
-  let beacons = [...scanners[0]];
+  let beacons = toMap(scanners[0]);
   const toProcess = scanners.slice(1);
   const scannerPositions: Point[] = [[0, 0, 0]];
   while (toProcess.length) {
     const scanner = toProcess.shift();
     let found = false;
-    rotation: for (const b of beacons) {
-      const aRebased = rebase(beacons, b);
+    rotation: for (const b of Object.values(beacons)) {
+      const aRebasedMap = toMap(rebase(Object.values(beacons), b));
       for (const bRotated of rotations(scanner)) {
         for (const rP of bRotated) {
-          const bRebased = rebase(bRotated, rP);
-          const inter = intersection(aRebased, bRebased);
-          if (inter.length > 2) {
-            const scannerPosition = b.map((v, i) => rP[i] - v);
-            scannerPositions.push(scannerPosition);
-            beacons = union(beacons, rebase(bRotated, scannerPosition));
-            found = true;
-            break rotation;
+          let count = 0;
+          for (const bR of rebase(bRotated, rP)) {
+            if (aRebasedMap[key(bR)]) {
+              count++;
+            }
+
+            if (count === 3) {
+              const scannerPosition = b.map((v, i) => rP[i] - v);
+              scannerPositions.push(scannerPosition);
+              for (const p of rebase(bRotated, scannerPosition)) {
+                beacons[key(p)] = p;
+              }
+              found = true;
+              break rotation;
+              }
           }
         }
       }
@@ -108,10 +109,10 @@ async function run() {
       toProcess.push(scanner);
     }
 
-    console.log(`found: ${beacons.length} beacons, to process: ${toProcess.length} scanners`);
+    console.log(`found: ${Object.keys(beacons).length} beacons, to process: ${toProcess.length} scanners`);
   }
 
-  console.log('Part 1:', beacons.length);
+  console.log('Part 1:', Object.keys(beacons).length);
 
   let max = 0;
   for (let i = 0; i < scannerPositions.length; i++) {
